3d9402595fc938383b30829efcd7f6236593f6d8
Dennis Yakovlev <dennis.yakovlev@yahoo.ca>
2023-02-26 12:41:58 -0500

    changed build script

--- run.sh
+++ run.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
+maybe_err()
+{
+    if [ $? -ne 0 ]; then
+        >&2 echo "\n\n\t$1\n\n"
+        exit 1
+    fi
+}
+
 build_dir=build_out
 
 mkdir -p "$build_dir"
 
 #configure
 cmake -B "$build_dir" -DTESTING=1 -DFAST_TESTS=true
-if [ $? -ne 0 ]; then
-    >&2 echo "cmake configuration failed"
-    exit 1
-fi
+maybe_err "cmake configuration failed"
 
 # attempt to build build tests
 cmake --build "$build_dir" --target BUILD_TESTS
-if [ $? -ne 0 ]; then
-    >&2 echo "build tests could not build"
-    exit 1
-fi
+maybe_err "build tests could not build"
 
 # attempt to build normal tests
 cmake --build "$build_dir" --target TESTS
-if [ $? -ne 0 ]; then
-    >&2 echo "tests could not build"
-    exit 1
-fi
+maybe_err "tests could not build"
 
+# run the tests
 ctest --test-dir "$build_dir" --verbose
-if [ $? -ne 0 ]; then
-    >&2 echo "tests failed to run"
-    exit 1
-fi
+maybe_err "tests failed to run"
